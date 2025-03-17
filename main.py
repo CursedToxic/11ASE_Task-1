f626c5b3ba2d08e2e426cf2eddee9f14a1e7085d
CursedToxic <160098130+CursedToxic@users.noreply.github.com>
2025-03-17 15:10:18 +1100

    Broke Dark Mode
    Tried to expand on the dark mode, now it doesn't work anymore.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root = tk.Tk()
 root.title("Window")
 root.configure(background = "white")
 root.minsize(200, 200)
-root.geometry("1280x720+500+250")
+root.geometry("1280x720+400+200")
 
 Text1 = Label(root, text= "This is to see if you are an idiot")
 Text2 = Label(root, text= "If you are reading this you have spent at least 1 second on this app")
@@ -14,13 +14,25 @@ Text2 = Label(root, text= "If you are reading this you have spent at least 1 sec
 Text1.pack()
 Text2.pack()
 
+def gullibility():
+      times = Button2.clicked()
+      print(f"This user has clicked the button {times} times")
+
 def darkMode():
     # darkText = Label(root, text= "Dark Mode not activated", fg="white")
-        darkText = root.configure(background= "black")
-        darkText.pack()
+        is_dark = root.cget("bg")
+        is_light = "white" if is_dark == "black" else "black"
+        darkText = root.configure(background= is_light)
+    # darkText.pack()
+        while root.configure(background= is_dark):
+                Button3 = tk.Button(root, text= "Light Mode", bg= "white", fg="black", command=darkMode)
+                if root.configure(background= is_light):
+                    pass 
+                else:
+                    continue
 
 Button1 = tk.Button(root, text= "Click to see if you are dumb")
-Button2 = tk.Button(root, text= "Click to see if you are gullible")
+Button2 = tk.Button(root, text= "Click to see if you are gullible", command=gullibility)
 Button3 = tk.Button(root, text= "Dark Mode", bg= "black", fg="white", command=darkMode)
 # Button1.grid(row=0, column=10)
 # Button2.grid(row=10, column=0)
